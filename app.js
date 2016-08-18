3c5173c07a3a20f21e64b5b268a3a67db726564d
jesse soldat <jsoldat@hotmail.com>
2016-08-18 16:29:30 +0700

    added bootstrap and jquery

--- app.js
+++ app.js
@@ -13,11 +13,15 @@ app.engine('dust', cons.dust);
 app.set('view engine', 'dust');
 app.set('views', __dirname + '/views');
 
-app.use(express.static(path.join(__dirname + 'public')));
+
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.get('/', function(req, res){
+	res.render('index');
+});
 
 app.listen(3000, function(){
 	console.log('listening on 3000');
